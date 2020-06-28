8fdd87964cf4a8236b825824cd9a9c82a217bd40
Scales <scales@scal.es>
2020-06-28 12:08:58 -0400

    Add basic in-game console (commands not implemented yet)

---
+++ HaloTAS/MCCTAS/tas_console.h
@@ -0,0 +1,5 @@
+#pragma once
+class tas_console
+{
+};
+
--- HaloTAS/MCCTAS/tas_overlay.cpp
+++ HaloTAS/MCCTAS/tas_overlay.cpp
@@ -15,8 +15,13 @@ static ID3D11DeviceContext* gD3DContext = nullptr;
 static ID3D11RenderTargetView* gRenderTargetView = nullptr;
 static IDXGISwapChain* gSwapChain = nullptr;
 static bool gShowMenu = false;
+static bool gShowConsole = false;
 static bool gIsInitD3D = false;
 static WNDPROC gOriginalWndProcHandler = nullptr;
+static int gWndWidth = 0, gWndHeight = 0;
+
+static std::vector<std::string> command_history;
+static char tas_command_line_buffer[512];
 
 LRESULT CALLBACK imgui_wndproc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -29,11 +34,44 @@ LRESULT CALLBACK imgui_wndproc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPara
 
 	io.MouseDrawCursor = gShowMenu;
 
+	RECT rect;
+	if (GetClientRect(hWnd, &rect)) {
+		gWndWidth = rect.right - rect.left;
+		gWndHeight = rect.bottom - rect.top;
+	}
+
+	static bool ignore_next_tilde = false;
+	if (uMsg == WM_KEYDOWN) {
+		if (wParam == VK_OEM_3) // VK_OEM_3 = TILDE
+		{
+			if (gShowConsole) {
+				gShowConsole = false;
+				ZeroMemory(tas_command_line_buffer, sizeof(tas_command_line_buffer));
+				ignore_next_tilde = true;
+				return true;
+			}
+		}
+	}
 	if (uMsg == WM_KEYUP)
 	{
-		if (wParam == VK_DELETE)
-		{
-			gShowMenu = !gShowMenu;
+		if (!gShowConsole) {
+			if (wParam == VK_DELETE)
+			{
+				gShowMenu = !gShowMenu;
+				return true;
+			}
+		}
+		if (!gShowMenu) {
+			if (wParam == VK_OEM_3) // VK_OEM_3 = TILDE
+			{
+				if (ignore_next_tilde) {
+					ignore_next_tilde = false;
+				}
+				else {
+					gShowConsole = !gShowConsole;
+					return true;
+				}
+			}
 		}
 	}
 
@@ -51,8 +89,18 @@ LRESULT CALLBACK imgui_wndproc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPara
 		case WM_INPUT:
 			ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam);
 			return true;
-			break;
 		default:
+			break;
+		}
+	}
+
+	if (gShowConsole) {
+		switch (uMsg) {
+		case WM_KEYDOWN:
+		case WM_KEYUP:
+		default:
+			ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam);
+			return true;
 			break;
 		}
 	}
@@ -68,12 +116,64 @@ void reset_wndproc_handler()
 }
 
 namespace tas::overlay {
+	void render_console() {
+		const float DISTANCE = 10.0f;
+		
+		ImGuiStyle& style = ImGui::GetStyle();
+		ImGuiIO& io = ImGui::GetIO();
+		
+		ImVec2 console_window_pos = ImVec2(DISTANCE, io.DisplaySize.y - DISTANCE);
+		ImVec2 window_pos_pivot = ImVec2(0.0f, 1.0f);
+		ImGui::SetNextWindowPos(console_window_pos, ImGuiCond_Always, window_pos_pivot);
+		ImGui::SetNextWindowBgAlpha(0.25f); // Transparent background
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoMove;
+		if (ImGui::Begin("MCCTAS Console", nullptr, window_flags))
+		{
+			ImGuiInputTextFlags command_line_flags = ImGuiInputTextFlags_EnterReturnsTrue;
+			ImGui::AlignTextToFramePadding();
+			//ImGui::Text("MCCTAS(");
+			ImGui::Text("halo(");
+			ImGui::SameLine();
+			ImGui::SetKeyboardFocusHere();
+			float consoleWidth = std::max<float>(static_cast<float>(gWndWidth - 180), 720.0f);
+			ImGui::SetNextItemWidth(consoleWidth);
+			if (ImGui::InputText("", tas_command_line_buffer, sizeof(tas_command_line_buffer), command_line_flags)) {
+				tas_logger::info("Executed MCCTAS Command: {}", tas_command_line_buffer);
+				std::string command(tas_command_line_buffer);
+				if (command.length() > 0) {
+					command_history.push_back(command);
+				}
+				ZeroMemory(tas_command_line_buffer, sizeof(tas_command_line_buffer));
+			}
+		}
+		ImGui::End();
+
+		ImVec2 history_window_pos = ImVec2(DISTANCE, io.DisplaySize.y - DISTANCE - 60);
+		ImGui::SetNextWindowPos(history_window_pos, ImGuiCond_Always, window_pos_pivot);
+		ImGui::SetNextWindowBgAlpha(0.25f); // Transparent background
+		if (ImGui::Begin("MCCTAS Console History", nullptr, window_flags)) {
+			for (auto& command : command_history) {
+				ImGui::Text(command.c_str());
+			}
+		}
+		ImGui::End();
+	}
+
 	void initialize(IDXGISwapChain* SwapChain)
 	{
 		ImGui::CreateContext();
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
 		io.WantCaptureMouse = true;
+		io.Fonts->AddFontFromFileTTF("consola.ttf", 30);
+		ImGuiStyle& style = ImGui::GetStyle();
+		style.ScaleAllSizes(1.5f);
+		style.WindowRounding = 0.0f;
+		style.WindowBorderSize = 0.0f;
+
 		ImGui::StyleColorsDark();
+
+		ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(1.0f, 0.0f, 1.0f, .05f));
+		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(ImColor(255, 0, 255)));
 
 		if (FAILED(SwapChain->GetDevice(__uuidof(ID3D11Device), (PVOID*)&gD3DDevice))) {
 			tas_logger::error(L"SwapChain->GetDevice failed");
@@ -124,6 +224,9 @@ namespace tas::overlay {
 
 		if (gShowMenu) {
 			ImGui::ShowDemoWindow();
+		}
+		if (gShowConsole) {
+			render_console();
 		}
 
 		ImGui::EndFrame();
